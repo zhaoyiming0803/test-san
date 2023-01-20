45d9f4795b6c8e10284b40b9b199bd3da6f3e0dc
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2023-01-20 21:37:23 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import App from './src/App'
 
+console.log('App: ', App)
+
 debugger
 const app = new App()
+console.log('app: ', app)
 
 const root = document.querySelector('#test-san-container')
 
